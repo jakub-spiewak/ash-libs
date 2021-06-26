d590a3ae6211aec84e3013708fbf7a93519e4722
Jakub Śpiewak <priv@jakubspiewak.com>
2021-06-26 12:09:07 +0200

    response polish

--- ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/mail/ApiResponseReceiveMail.java
+++ ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/mail/ApiResponseReceiveMail.java
@@ -3,14 +3,14 @@ package com.jakubspiewak.ashapimodellib.model.mail;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 @Builder
 public class ApiResponseReceiveMail {
-    private MailConfiguration configuration;
     private String from;
     private String subject;
-    private String receiptDate;
+    private Date receiptDate;
     private List<MailAttachment> attachments;
 }
