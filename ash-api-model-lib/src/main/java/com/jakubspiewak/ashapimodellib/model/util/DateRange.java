d434d4b982e400c1558878ad3263182f3dc0a7d2
Jakub Śpiewak <priv@jakubspiewak.com>
2021-06-27 22:15:01 +0200

    iso date format

--- ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/util/DateRange.java
+++ ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/util/DateRange.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
+
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
 
 @Data
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor 
+@AllArgsConstructor
 public class DateRange {
-    private Date min;
-    private Date max;
+    private @DateTimeFormat(iso = DATE) Date min;
+    private @DateTimeFormat(iso = DATE) Date max;
 }
