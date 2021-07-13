98c777cb5a631ec59194385c9c3bfc9b9bad7cf8
Jakub Śpiewak <priv@jakubspiewak.com>
2021-07-13 22:54:01 +0200

    document create request

---
+++ ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/document/ApiDocumentCreateRequest.java
@@ -0,0 +1,18 @@
+package com.jakubspiewak.ashapimodellib.model.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiDocumentCreateRequest {
+    private UUID ownerId;
+    private String fileName;
+    private byte[] file;
+}
