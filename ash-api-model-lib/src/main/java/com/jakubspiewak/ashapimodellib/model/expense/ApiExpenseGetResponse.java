61b01c1a80a54eafdbbf972eed9dd58a893f4891
Jakub Śpiewak <priv@jakubspiewak.com>
2021-08-18 12:33:08 +0200

    id to response

--- ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/expense/ApiExpenseGetResponse.java
+++ ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/expense/ApiExpenseGetResponse.java
@@ -5,11 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApiExpenseGetResponse {
+  private UUID id;
+
   private String name;
 
   private Float amount;
--- ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/expense/MailExpenseConfig.java
+++ ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/expense/MailExpenseConfig.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MailExpenseConfig {
+
   private String mailAddress;
+
   private String attachmentPattern;
 }
