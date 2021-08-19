91ce41b240117277c4c1f7fa64120d15610dd961
Jakub Śpiewak <priv@jakubspiewak.com>
2021-08-19 06:43:09 +0200

    use LocalDate

--- ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/document/ApiDocumentCreateRequest.java
+++ ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/document/ApiDocumentCreateRequest.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.UUID;
 
@@ -14,7 +15,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ApiDocumentCreateRequest {
 
-    private Date date;
+    private LocalDate date;
 
     private String type;
 
--- ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/document/ApiDocumentGetResponse.java
+++ ash-api-model-lib/src/main/java/com/jakubspiewak/ashapimodellib/model/document/ApiDocumentGetResponse.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.UUID;
 
@@ -16,7 +17,7 @@ public class ApiDocumentGetResponse {
 
     // TODO: Add company info etc.
 
-    private Date date;
+    private LocalDate date;
 
     private UUID fileId;
 
