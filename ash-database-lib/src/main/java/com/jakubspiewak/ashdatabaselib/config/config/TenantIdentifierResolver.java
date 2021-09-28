fa9f39197170fcb93e4b3ce83eea4f4dddb79c22
Jakub Śpiewak <priv@jakubspiewak.com>
2021-09-29 00:00:30 +0200

    remove request scope from identifier resolver

--- ash-database-lib/src/main/java/com/jakubspiewak/ashdatabaselib/config/config/TenantIdentifierResolver.java
+++ ash-database-lib/src/main/java/com/jakubspiewak/ashdatabaselib/config/config/TenantIdentifierResolver.java
@@ -2,14 +2,11 @@ package com.jakubspiewak.ashdatabaselib.config.config;
 
 import com.jakubspiewak.ashdatabaselib.config.DatabaseContextHolder;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver {
   @Override
   public String resolveCurrentTenantIdentifier() {
